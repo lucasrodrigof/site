978cfc654028c77d01bba0c0965282727e7be402
Lucas Rodrigo <Lucas.moreira34@fatec.sp.gov.br>
2022-04-12 15:43:17 -0300

    Update index.php

--- codigos/Site TV Taubaté/index.php
+++ codigos/Site TV Taubaté/index.php
@@ -5,36 +5,65 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
     <link rel="stylesheet" href="css\bootstrap.min.css">
+    <link rel="stylesheet" href="css\style.css">
+    
 </head>
+
 <body>
-<div class="container">
-    <div class="row">
-        <div class="col-6">
-    <h1>Cadastro</h1>
-    <form action="">
-    <div class="form-group">
-        <label for="nome">Nome:</label>
-        <input class="form-control" type="text" name="nome" id="nome" placeholder="Digite seu nome...">
-    </div>
-    <div class="form-group">
-        <label for="email">E-mail:</label>
-        <input class="form-control" type="email" name="email" id="email" placeholder="Digite seu e-mail...">
-    </div>
-    <div class="form-group">
-        <label for="rg">RG:</label>
-        <input class="form-control" type="number" name="rg" id="rg" placeholder="Digite seu RG...">
-        <div id="rg" class="form-text">Somente os números do RG</div>
-    </div>
-    <div class="form-group">
-        <label for="cpf">CPF:</label>
-        <input class="form-control" type="number" name="cpf" id="cpf" placeholder="Digite seu CPF...">
-        <div id="cpf" class="form-text">Somente os números do CPF</div>
-    </div>
-    </form>
+    <header>
+        <nav class="navbar fixed-top navbar-light" style="background-color: #00bfff;"> 
+                <a class="navbar-brand" href="index.php">
+                <img src="Bandeira_de_Taubaté.png" alt="Bandeira de Taubaté" width="50" height="40">
+                Início
+                </a>
+                
+        </nav>
+    </header>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 offset-md-3" >
+                    <div class="card">
+                        <div class="card-body" style="background-color: #cbd9ea;">
+                            <h1>Cadastro Inicial</h1>
+                            <form id="form-contato">
+                                <div class="form-group">
+                                    <label for="nome">Nome:</label>
+                                    <input class="form-control" type="text" name="nome" id="nome" placeholder="Digite seu nome completo...">
+                                </div>
+                                
+                                <div class="form-group">
+                                    <label for="email">E-mail:</label>
+                                    <input class="form-control" type="email" name="email" id="email" placeholder="Digite seu e-mail...">
+                                    <div id="email" class="form-text">Este será seu usuário</div>
+                                </div>
+                
+                                <div class="form-group">
+                                    <label for="senha">Senha:</label>
+                                    <input class="form-control" type="text" name="senha" id="senha" placeholder="Digite sua senha...">
+                                    
+                                </div>
+                                
+                                <div class="form-group text-center">
+                                <button type="submit" class="btn btn-primary">
+                                <a href="cadastro.php">
+                                Enviar
+                                </button>
+                                </a>
+                                <button type="reset" class="btn btn-danger">Cancelar</button>
+                                </div>
+                                
+                                <div class="alert alert-danger d-none">
+                                Preencha o campo
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
-<script src="js/jquery-3.3.1.slim.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
+
+    <script src="js/jquery-3.3.1.slim.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script scr="js/formulario.js"></script>
 </body>
 </html>
