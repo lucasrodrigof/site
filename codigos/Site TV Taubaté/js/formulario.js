74cfde73f0e06c6fbd1ca54c373700812b494069
Lucas Rodrigo <Lucas.moreira34@fatec.sp.gov.br>
2022-04-21 00:17:01 -0300

    Add files via upload

--- codigos/Site TV Taubaté/js/formulario.js
+++ codigos/Site TV Taubaté/js/formulario.js
@@ -1,13 +1,31 @@
 $('#form-contato').submit(function(){
-    var nome = $('nome');
+    var celular = $('#celular');
+    var senha = $('#senha');
     var erro = $('.alert');
-
-    if(nome.val() == ''){
-        erro.removeClass('d-none');    
-
-        return false;
+    var campo = $('#campo-erro');
+  
+    // removendo o elemento da tela sempre que tentar submeter o formulario
+    erro.addClass('d-none');
+    $('.is-invalid').removeClass('is-invalid');
+  
+    // valida o campo celular
+    if (celular.val() == '') {
+      erro.removeClass('d-none');
+      campo.html('celular'); // nome do campo que não foi preenchido!
+      celular.focus();
+      celular.addClass('is-invalid');
+      return false;
     }
-
-    // Vai ficar aqui por enquanto
-    return false;
-});
+    // valida o campo e-mail
+    if (senha.val() == '') {
+      erro.removeClass('d-none');
+      campo.html('e-mail'); // nome do campo que não foi preenchido!
+      senha.focus();
+      senha.addClass('is-invalid');
+      return false;
+    }  
+    
+    
+    // se chegar aqui pode enviar os dados!
+    return true;
+  });
