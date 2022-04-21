4d54f6da82dee2ff9ad185d3666684882affcc4a
Lucas Rodrigo <Lucas.moreira34@fatec.sp.gov.br>
2022-04-21 00:16:01 -0300

    Add files via upload

--- codigos/Site TV Taubaté/cadastro.php
+++ codigos/Site TV Taubaté/cadastro.php
@@ -4,34 +4,114 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
-    <link rel="stylesheet" href="css\bootstrap.min.css">
-    <link rel="stylesheet" href="css\style.css">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="stylecadastro.css">
+    
+    <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="js/jquery-3.3.1.slim.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.11/jquery.mask.min.js"></script>
+    
+    
+    
 </head>
+
 <body>
-<div class="container">
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-light">
+          <img src="Bandeira_de_Taubaté.png" width="57" height="40" alt="">     
+          <a class="navbar-brand" >   
+          RG Taubaté
+          </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          
+          <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+            
+          <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Início</a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Entrar </a>
+              </li>
+              
+              <li class="nav-item active">
+                <a class="nav-link" href="index.php">Registrar</a>
+              </li>
+
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Eventos</a>
+              </li>
+
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Sobre Nós</a>
+              </li>
+
+              
+            </ul>
+          </div>
+          
+      </nav>
+    <header>
+
+    <div class="container">
     <div class="row">
-        <div class="col-6">
-            <div class="card">
-                <div class="card-body">
-                    <h1>Cadastro Completo</h1>
-                    <form action="">
-                        <div class="form-group">
-                            <label for="rg">RG:</label>
-                            <input class="form-control" type="number" name="rg" id="rg" placeholder="Digite seu RG...">
-                            <div id="rg" class="form-text">Somente números</div>
-                        </div>
-                        <div class="form-group">
-                            <label for="cpf">CPF:</label>
-                            <input class="form-control" type="number" name="cpf" id="cpf" placeholder="Digite seu CPF...">
-                            <div id="cpf" class="form-text">Somente números</div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+    <div class="col-6">
+    <div class="card">
+    <div class="card-body">
+
+    <form id="form-cadastro">
+    <div class="mb-3">
+    <h1>Cadastro Completo:</h1>
+    <label class="form-label">Nome Completo:</label>
+    <input type="text" class="form-control" id="nome" required>
     </div>
-</div>
-<script src="js/jquery-3.3.1.slim.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
+
+    <div class="mb-3">
+    <label class="form-label">Telefone:</label>
+    <input type="text" class="form-control" id="telefone">
+    </div>
+
+    <div class="mb-3">
+    <label class="form-label">RG:</label>
+    <input type="text" class="form-control" id="rg">
+    </div>
+
+    <div class="mb-3">
+    <label class="form-label">CPF:</label>
+    <input type="text" class="form-control" id="cpf">
+    </div>
+
+    <div class="form-group text-center">
+    <button type="submit" class="btn btn-primary">Enviar</button>
+    <button type="reset" class="btn btn-danger">Cancelar</button>
+    </div>
+    
+
+    </form>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+
+<!-- Máscara celular-->
+<script>
+    
+    $("#telefone").mask("(99) 9999-9999"); 
+
+    $("#rg").mask("99.999.999-9");
+
+    $("#cpf").mask("999999999/99");
+
+</script>
 </body>
 </html>
--- codigos/Site TV Taubaté/index.php
+++ codigos/Site TV Taubaté/index.php
@@ -4,66 +4,105 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
-    <link rel="stylesheet" href="css\bootstrap.min.css">
-    <link rel="stylesheet" href="css\style.css">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    
+    <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="js/jquery-3.3.1.slim.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.11/jquery.mask.min.js"></script>
+    
     
 </head>
 
+
 <body>
     <header>
-        <nav class="navbar fixed-top navbar-light" style="background-color: #00bfff;"> 
-                <a class="navbar-brand" href="index.php">
-                <img src="Bandeira_de_Taubaté.png" alt="Bandeira de Taubaté" width="50" height="40">
-                Início
-                </a>
-                
-        </nav>
-    </header>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 offset-md-3" >
-                    <div class="card">
-                        <div class="card-body" style="background-color: #cbd9ea;">
-                            <h1>Cadastro Inicial</h1>
-                            <form id="form-contato">
-                                <div class="form-group">
-                                    <label for="nome">Nome:</label>
-                                    <input class="form-control" type="text" name="nome" id="nome" placeholder="Digite seu nome completo...">
-                                </div>
-                                
-                                <div class="form-group">
-                                    <label for="email">E-mail:</label>
-                                    <input class="form-control" type="email" name="email" id="email" placeholder="Digite seu e-mail...">
-                                    <div id="email" class="form-text">Este será seu usuário</div>
-                                </div>
-                
-                                <div class="form-group">
-                                    <label for="senha">Senha:</label>
-                                    <input class="form-control" type="text" name="senha" id="senha" placeholder="Digite sua senha...">
-                                    
-                                </div>
-                                
-                                <div class="form-group text-center">
-                                <button type="submit" class="btn btn-primary">
-                                <a href="cadastro.php">
-                                Enviar
-                                </button>
-                                </a>
-                                <button type="reset" class="btn btn-danger">Cancelar</button>
-                                </div>
-                                
-                                <div class="alert alert-danger d-none">
-                                Preencha o campo
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+      <nav class="navbar navbar-expand-lg navbar-light">
+          <img src="Bandeira_de_Taubaté.png" width="57" height="40" alt="">     
+          <a class="navbar-brand" >   
+          RG Taubaté
+          </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          
+          <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+            
+          <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Início</a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Entrar </a>
+              </li>
+              
+              <li class="nav-item active">
+                <a class="nav-link" href="index.php">Registrar</a>
+              </li>
 
-    <script src="js/jquery-3.3.1.slim.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script scr="js/formulario.js"></script>
-</body>
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Eventos</a>
+              </li>
+
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Sobre Nós</a>
+              </li>
+
+              
+            </ul>
+          </div>
+          
+      </nav>
+    <header>
+
+    <div class="container">
+    <div class="row">
+    <div class="col-6">
+    <div class="card">
+    <div class="card-body">
+      
+
+    <form id="form-cadastro">
+    <div class="mb-4">
+    <h1>Cadastro:</h1>
+    <label class="form-label">Celular:</label>
+    <input type="text" class="form-control" id="celular" required>
+    
+    <div class="form-text">Este será seu usuário.</div>
+    </div>
+
+    <div class="mb-3">
+    <label class="form-label">Senha:</label>
+    <input type="password" class="form-control" id="senha" minlength="8" required>
+    </div>
+
+    <div class="form-group text-center">
+    <a class="btn btn-primary" href="cadastro.php">Enviar</a>
+    <button type="reset" class="btn btn-danger">Cancelar</button>
+    </div>
+
+    <div class="alert alert-danger d-none">
+    Preencha o campo <span id="campo-erro"></span>!
+    </div>
+    
+
+    </form>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    
+  <!-- Máscara celular-->
+  <script>
+    $("#celular").mask("(99)99999-9999");
+  </script>
+  </body>
 </html>
